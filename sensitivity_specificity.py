db685c44a2325c2fbe87d5e17e6abd78078207bb
Upakar Paudel <upakarpaudel96@gmail.com>
2017-11-13 16:43:02 +0545

    With precision and f-Measures

--- sensitivity_specificity.py
+++ sensitivity_specificity.py
@@ -8,6 +8,11 @@ def true_false(cm):
     sensitivity = true_positive / (true_positive + false_negative)
     specificity = true_negative / (true_negative + false_positive)
 
+    precision = true_positive / (true_positive + false_positive)
+    fMeasure = 2*true_positive / (2*true_positive + false_positive + false_negative)
+
     print("Sensitivity is", sensitivity)
     print("Specificity is", specificity)
+    print("Precision is", precision)
+    print("fMeasure is", fMeasure)
 
